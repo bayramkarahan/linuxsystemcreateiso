e265f64b16dcb98182682c2c685909083a72578d
karahan <bayramk@gmail.com>
2022-08-05 21:56:56 +0300

    mebsertifika

--- 3_0_sertifika.sh
+++ 3_0_sertifika.sh
@@ -2,7 +2,7 @@
 #### meb sertifikası
 
 
-wget http://sertifika.meb.gov.tr/MEB_SERTIFIKASI.cer -O /tmp/MEB_SERTIFIKASI.cer
-openssl x509 -inform DER -in /tmp/MEB_SERTIFIKASI.cer -out /tmp/MEB_SERTIFIKASI.crt
+#wget http://sertifika.meb.gov.tr/MEB_SERTIFIKASI.cer -O /tmp/MEB_SERTIFIKASI.cer
+openssl x509 -inform DER -in MEB_SERTIFIKASI.cer -out /tmp/MEB_SERTIFIKASI.crt
 cp /tmp/MEB_SERTIFIKASI.crt chroot/usr/local/share/ca-certificates/MEB_SERTIFIKASI.crt
 chroot chroot update-ca-certificates
